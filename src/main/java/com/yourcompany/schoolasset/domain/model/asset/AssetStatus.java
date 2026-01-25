0579a7832b183b34c5e6858d72b3c550dfc25dda
yuto sakakibara <emukei007@gmail.com>
2026-01-25 12:22:38 +0900

    Update AssetStatus.java

--- src/main/java/com/yourcompany/schoolasset/domain/model/asset/AssetStatus.java
+++ src/main/java/com/yourcompany/schoolasset/domain/model/asset/AssetStatus.java
@@ -8,10 +8,9 @@ public enum AssetStatus {
     LOST,        // 紛失
     MAINTENANCE;  // メンテナンス中
 
-    // TODO [ASSET-101] この個体がどのモデル（MacBook等）に属するか
-    // 本来は EquipmentModel エンティティ等と紐付けますが、今は String で通します
+    // この個体がどのモデル（MacBook等）に属する
     private String model;
 
-    // TODO [ASSET-102] 管理番号（バーコード値など）
+    // 管理番号（バーコード値など）
     private String assetCode;
 }
